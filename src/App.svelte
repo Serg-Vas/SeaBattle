bdd5c30f6609a11d87e296a710cd09fc5a796b9f
balladaosoldate <balladaosoldate@gmail.com>
2022-08-20 16:28:55 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Cage from "./lib/Cage.svelte"
+  import {key} from "./lib/utilt.js"
+  import { setContext } from 'svelte';
+
+  let table = [0,1,0,1,0,1,0,1,0,1]
+  setContext(key,{
+    getTable:() => table
+  })
+
+  function tap(e){
+  if (table[e.target.innerText] == 0){
+    table[e.target.innerText] = 1
+    e.target.innerText = "X"
+  }
+  console.log(table);
+}
+
+function test(event) {
+  console.log(table)
+}
+</script>
+
+<button on:click={test}>test</button>
+
+
+{#each table as item, i}
+  <Cage i={i} item={item}></Cage>
+{/each}
---
+++ src/lib/Cage.svelte
@@ -0,0 +1,19 @@
+<script>
+import { getContext } from "svelte";
+import { key } from "./utilt";
+
+
+    export let i
+    export let item
+
+    let arr = getContext(key)
+
+    function tap(event) {
+        item = (item === 1) ? -1 : -2
+        event.target.disabled = true
+        let table = arr.getTable()
+        table[i] = item 
+        console.log(table);
+    }
+</script>
+<button on:click={tap}> {item} </button>
